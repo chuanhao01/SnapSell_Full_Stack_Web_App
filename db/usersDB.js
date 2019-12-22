318576c587c5b15709045d1870dcdf0abb46be47
chuanhao01 <chuanhaolim@gmail.com>
2019-12-22 18:50:40 +0800

    Removing old code from previous

--- db/usersDB.js
+++ db/usersDB.js
@@ -80,31 +80,6 @@ const usersDB = {
             }.bind(this)
         );
     },
-    // Get the user by the username from the db, returns the whole user
-    // getUserByUsername(username){
-    //     return new Promise((resolve, reject) => {
-    //         this.pool.query(`
-    //         SELECT * FROM USERS
-    //         WHERE ((username = ?) AND (deleted = 0))
-    //         `, [username], function(err, data){
-    //             if(err){
-    //                 // If there is SQL errors
-    //                 return reject(err);
-    //             }
-    //             else if(data.length === 0){
-    //                 const err = new Error('User does not exist');
-    //                 err.code = 'USER_NOT_EXIST';
-    //                 return reject(err);
-    //             }
-    //             else if(data.length > 1){
-    //                 const err = new Error('Too many users');
-    //                 err.code = 'MANY_USERS';
-    //                 return reject(err);
-    //             }
-    //             return resolve(data[0]);
-    //         });
-    //     });
-    // },
     // Get the user by the username from the db
     getUserByUsername(username){
         return new Promise((resolve, reject) => {
